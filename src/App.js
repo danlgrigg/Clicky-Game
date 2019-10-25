be29e34c97ae9f4e9d2301e26ff737e5390f0eb9
danlgrigg <griggdl@gmail.com>
2019-10-25 15:40:52 -0700

    updated folder structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      </header>
+
+      <Children name={""}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pictures.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Children(props) {
+  console.log(props);
+  return (
+    
+  );
+}
+
+export default Children;
