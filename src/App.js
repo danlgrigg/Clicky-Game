e3e9177005d2548b8bca9459462d625fe8485252
danlgrigg <griggdl@gmail.com>
2019-10-27 23:57:25 -0700

    Set default state.  Created function shuffle images on start.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React from 'react';
+import React, {component} from 'react';
+import Images from "./components/Images.json";
 import Header from './components/Header';
 import Jumbotron from './components/Jumbotron';
-import cardContainer from './components/cardContainer';
+import CardContainer from './components/CardContainer';
 import Footer from './components/Footer';
 import './App.css';
-import Pictures from "./components/Pictures.json";
 
-// class App extends Component {
-  
-//   state = {
-//     Pictures
-//   };
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header/>
+//       <Jumbotron/>
+//       <CardContainer/>
+//       {/* <Footer/> */}
+//     </div>
+//   );
 // }
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Jumbotron/>
-      <Container/>
-      {/* <Children name={""}/> */}
-      {/* <Footer/> */}
-    </div>
-  );
+function shuffleImages(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
+class App extends Component {
+  // Set this.state
+  state = {
+    images,
+    currentScore: 0,
+    topScore: 0,
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+  
+}
 export default App;
